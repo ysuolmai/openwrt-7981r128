64d0e6ea42c80d76130f70f69a4b45054c04ab07
shmily103 <supanshui@gmail.com>
2024-11-07 02:11:09 +0800

    create new

---
+++ diy/diy-part2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+#add packages
+git clone https://github.com/shmily103/Panzy.git package/Panzy
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+# Modify WiFi ON
+sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# Modify default SSID
+sed -i 's/ssid=OpenWrt/ssid=Panzy/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# Modify Automount
+\cp -rf package/Panzy/mount.hotplug package/system/fstools/files
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+sed -i 's/OpenWrt/Panzy/g' package/base-files/files/bin/config_generate
+
+# Modify LAN port
+#sed -i 's/eth0/eth6/g' package/base-files/files/etc/board.d/99-default_network
+
