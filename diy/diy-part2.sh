9015a202e622d521029bbae8c30bb12d765abefb
shmily103 <supanshui@gmail.com>
2024-11-09 11:57:08 +0800

    Update diy-part2.sh

--- diy/diy-part2.sh
+++ diy/diy-part2.sh
@@ -8,7 +8,7 @@
 # Blog: https://p3terx.com
 #============================================================
 #add packages
-git clone https://github.com/shmily103/Panzy.git package/Panzy
+#git clone https://github.com/shmily103/Panzy.git package/Panzy
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
@@ -17,11 +17,11 @@ sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac802
 # Modify default SSID
 sed -i 's/ssid=OpenWrt/ssid=Panzy/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # Modify Automount
-\cp -rf package/Panzy/mount.hotplug package/system/fstools/files
+#\cp -rf package/Panzy/mount.hotplug package/system/fstools/files
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 # Modify hostname
-sed -i 's/OpenWrt/Panzy/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/Panzy/g' package/base-files/files/bin/config_generate
 # Modify LAN port
 #sed -i 's/eth0/eth6/g' package/base-files/files/etc/board.d/99-default_network
 # Modify patch
